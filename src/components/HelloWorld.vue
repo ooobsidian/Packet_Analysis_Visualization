8beda031611402e22720bec63b7088c1d1886ea8
ooobsidian <obsidian_lrx@aliyun.com>
2018-10-15 22:35:38 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="hello">
+    <Split v-model="split3">
+      <div slot="left" class="demo-split-pane">
+        <Button type="primary" size="large" long>获取网卡信息</Button>
+        <div style="display: flex;margin: 20px 0">
+          <Input v-model="value1" size="large" class="inputid" placeholder="输入序号"/>
+          <Button type="primary" size="large">获取</Button>
+        </div>
+        <Button type="primary" size="large" long>图表展示</Button>
+      </div>
+      <div slot="right" class="demo-split-pane no-padding">
+        <Split v-model="split4" mode="vertical">
+          <div slot="top" class="demo-split-pane">
+            <Table stripe :columns="columns1" :data="data1"></Table>
+          </div>
+          <div slot="bottom" class="demo-split-pane">
+            Bottom Pane
+          </div>
+        </Split>
+      </div>
+
+    </Split>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        split3: 0.15,
+        split4: 0.9,
+        columns1: [
+          {
+            title: '序号',
+            key: 'id'
+          },
+          {
+            title: '名称',
+            key: 'name'
+          },
+          {
+            title: 'Address1',
+            key: 'address'
+          },
+          {
+            title: 'Address2',
+            key: 'otherAddress'
+          }
+        ],
+        data1: [
+          {
+            id: 1,
+            name: "wlp5s0",
+            address: "192.168.50.223",
+            otherAddress: "fe80:0:0:0:b00b:2a93:319e:bc76"
+          },
+          {
+            id: 2,
+            name: "any",
+            address: "192.168.50.223",
+            otherAddress: "fe80:0:0:0:b00b:2a93:319e:bc76"
+          },
+          {
+            id: 3,
+            name: "lo",
+            address: "127.0.0.1",
+            otherAddress: "0:0:0:0:0:0:0:1"
+          },
+          {
+            id: 4,
+            name: "enp3s0",
+            address: "127.0.0.1",
+            otherAddress: "0:0:0:0:0:0:0:1"
+          },
+          {
+            id: 5,
+            name: "bluetooth0",
+            address: "127.0.0.1",
+            otherAddress: "0:0:0:0:0:0:0:1"
+          },
+          {
+            id: 6,
+            name: "nflog",
+            address: "127.0.0.1",
+            otherAddress: "0:0:0:0:0:0:0:1"
+          },
+          {
+            id: 7,
+            name: "nfqueue",
+            address: "127.0.0.1",
+            otherAddress: "0:0:0:0:0:0:0:1"
+          },
+          {
+            id: 8,
+            name: "usbmon1",
+            address: "127.0.0.1",
+            otherAddress: "0:0:0:0:0:0:0:1"
+          },
+          {
+            id: 9,
+            name: "usbmon2",
+            address: "127.0.0.1",
+            otherAddress: "0:0:0:0:0:0:0:1"
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello {
+    height: 100%;
+  }
+
+  .inputid {
+    margin-right: 10px;
+  }
+
+  .demo-split-pane {
+    padding: 10px;
+  }
+
+  .demo-split-pane.no-padding {
+    height: 600px;
+    padding: 0;
+  }
+</style>
---
+++ src/components/root.vue
@@ -0,0 +1,66 @@
+<template>
+  <div style="height: 100%;">
+    <div class="pageWarpper">
+      <div class="title">计算机网络第三次研讨</div>
+      <div class="subtitle">Ipv4抓包分析</div>
+      <div class="name">16121670-李瑞轩 16121666-徐文 16121689-台枫</div>
+      <Button class="button" type="primary" shape="circle" size="large" long to="/HelloWorld">开始</Button>
+      <div class="date">2018.10.18</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "root",
+  }
+</script>
+
+<style>
+  .pageWarpper {
+    width: 40%;
+    height: 100%;
+    margin: 0 auto;
+  }
+
+  .title {
+    margin-top: 25%;
+    text-align: center;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;;
+    font-size: 60px;
+  }
+
+  .subtitle {
+    margin-top: 2%;
+    text-align: center;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;;
+    font-size: 40px;
+  }
+
+  .name {
+    margin-top: 3%;
+    text-align: center;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;;
+    font-size: 23px;
+  }
+
+  .date {
+    margin-top: 25%;
+    text-align: center;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;;
+    font-size: 20px;
+  }
+
+  .button {
+    margin-top: 6%;
+  }
+
+  .ivu-btn-long {
+    height: 55px !important;
+    line-height: 40px;
+  }
+
+  .ivu-btn-large {
+    font-size: 20px !important;
+  }
+</style>
